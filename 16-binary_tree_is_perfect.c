5233dc67d9d8cdd10af1e70e290392bc6ad4aa49
root <abdallahfatima363@gmail.com>
2022-11-17 17:11:50 -0800

    Task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,11 +1,63 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_is_perfect - Checks if a binary tree is perfect.
+ * @tree: A pointer to the root node of the tree to check.
+ *
+ * Return: If tree is NULL or not perfect, 0.
+ *         Otherwise, 1.
+ */
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
+	size_t l_size = 0, r_size = 0;
+
+	l_size = tree->left ? binary_tree_size(tree->left) : 0;
+	r_size = tree->right ? binary_tree_size(tree->right) : 0;
+	return (l_size + r_size + 1);
+}
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	size_t l_Height = 0, r_Height = 0;
+
+	l_Height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	r_Height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+	if (l_Height >= r_Height)
+		return (l_Height);
 	else
+		return (r_Height);
+}
+
+int power(int base, int exp)
+{
+	int power = 1, i = 1;
+
+	for (i = 1; i <= exp; i++)
 	{
-		size_t l_size = 0, r_size(tree->left) : 0;
-		r-size =tree->right ? binary_tree_size(tree->right) :0;
-		l_size = tree->left ? 
+		power = power * base;
+	}
+	return (power);
+}
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int size = 0, height = 0, nodes = 0;
+
+	if (tree)
+	{
+		height = binary_tree_height(tree) + 1;
+		size = binary_tree_size(tree);
+		nodes = power(2, height);
+		if (size == (nodes - 1))
+			return (1);
+		else
+			return (0);
+	}
+	return (0);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -83,5 +83,7 @@ heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
+int power(int base, int exp);
+
 
 #endif /* BINARY_TREES_H */
